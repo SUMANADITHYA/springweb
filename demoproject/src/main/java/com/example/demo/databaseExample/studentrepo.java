bcb7edaf1ce6b57565dbe8cf75207fc04bac014c
SUMANADITHYA <113884434+SUMANADITHYA@users.noreply.github.com>
2023-03-02 09:34:01 +0530

    Delete demoproject directory

--- demoproject/src/main/java/com/example/demo/databaseExample/studentrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.databaseExample;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.databaseExample.entity.Entity;
-
-public interface studentrepo extends JpaRepository<Entity, Integer>{
-	
-}
